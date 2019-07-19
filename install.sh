c4a14a004aeaecd31309079f7b7ed728093f7204
Ubuntu <hujk@VM.1jmqerx5seeu5f0hhih02ce25e.mx.internal.cloudapp.net>
2019-07-19 08:04:08 +0000

    add nodejs

--- install.sh
+++ install.sh
@@ -6,13 +6,19 @@ echo "Install & update"
 apt-get -y update ; apt-get -y dist-upgrade
 apt-get -y install apt-utils runit locales openssh-server cron vim git sudo rsync syslog-ng nginx apache2 software-properties-common
 apt-get -y install mysql-client mysql-server
-apt-get -y install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
+apt-get -y install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap 
+apt-get -y php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
 apt-get -y install fish zsh tmux htop thefuck wget curl aria2 lsof tree ncdu
 apt-get -y install golang default-jdk python-pip python-setuptools python3 python3-pip python3-dev g++ gcc lua50 perl 
 pip3 install --upgrade git+https://github.com/arthaud/python3-pwntools.git speedtest-cli
 apt-get -y install net-tools traceroute iputils-* p7zip-full p7zip-rar 
-apt-get -y install ack atop autoconf bison build-essential byobu calcurse cloc cowsay dirmngr dstat dtach duplicity emacs gawk gdb git-core glances gnupg2 iftop ipcalc iptraf ledger lftp libsqlite3-dev libssl-dev libtool  mc mtr multitail netcat nethogs netpipes nikto nmap nnn oysttyer parallel pgadmin3 polipo postgresql powertop pydf qalc qbittorrent-nox ranger remind rsstail rsyncrypto rtorrent screen searchandrescue siege silversearcher-ag sl slashem slurm socat sqlite3 tig tor tor-geoipdb torsocks tpp tsung ttyrec vifm wyrd yersinia zlib1g-dev zlib1g-dev
-apt-get -y autoremove ; apt-get autoclean
+apt-get -y install ack atop autoconf bison build-essential byobu calcurse cloc cowsay dirmngr dstat dtach duplicity 
+apt-get -y emacs gawk gdb git-core glances gnupg2 iftop ipcalc iptraf ledger lftp libsqlite3-dev libssl-dev libtool 
+apt-get -y mc mtr multitail netcat nethogs netpipes nikto nmap nnn oysttyer parallel pgadmin3 polipo postgresql powertop 
+apt-get -y pydf qalc qbittorrent-nox ranger remind rsstail rsyncrypto rtorrent screen searchandrescue siege silversearcher-ag 
+apt-get -y sl slashem slurm socat sqlite3 tig tor tor-geoipdb torsocks tpp tsung ttyrec vifm wyrd yersinia zlib1g-dev zlib1g-dev
+wget -O - https://deb.nodesource.com/setup_12.x | bash
+apt-get install -y nodejs
 wget https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb -P /
 dpkg -i /ripgrep_11.0.1_amd64.deb ; rm /ripgrep_11.0.1_amd64.deb
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O /packages-microsoft-prod.deb
@@ -43,7 +49,7 @@ ln -s ../home/root/.rootfs/etc/rc.local /etc/rc.local
 echo "eval \"\$(thefuck --alias)\"" >> ~/.bashrc
 echo "eval \"\$(thefuck --alias)\"" >> ~/.zshrc
 echo "thefuck --alias | source" >> /etc/fish/config.fish 
-chsh -s /bin/zsh root
+
 
 cd /tmp
 mv sshd_config /etc/ssh/
